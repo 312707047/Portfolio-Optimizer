1e625c8b3c20383a3f6987503807afb73b86fe81
novis10813 <zhizhongz895253@gmail.com>
2022-04-23 13:18:25 +0800

    batchnorm

--- agent/latent_space/Autoencoder.py
+++ agent/latent_space/Autoencoder.py
@@ -10,12 +10,21 @@ class Encoder(nn.Module):
         self.conv_2 = nn.Conv2d(64, 32, (1, 7), padding=0)
         self.conv_3 = nn.Conv2d(32, 16, (1, 10), padding=0)
         self.conv_4 = nn.Conv2d(16, 3, (1, 14), padding=0)
+        
+        self.bn_1 = nn.BatchNorm2d(64)
+        self.bn_2 = nn.BatchNorm2d(32)
+        self.bn_3 = nn.BatchNorm2d(16)
+        self.bn_4 = nn.BatchNorm2d(3)
     
     def forward(self, x):
         x = torch.relu(self.conv_1(x))
+        x = self.bn_1(x)
         x = torch.relu(self.conv_2(x))
+        x = self.bn_2(x)
         x = torch.relu(self.conv_3(x))
+        x = self.bn_3(x)
         x = torch.relu(self.conv_4(x))
+        x = self.bn_4(x)
         
         return x
 
@@ -29,12 +38,21 @@ class Decoder(nn.Module):
         self.convt_2 = nn.ConvTranspose2d(16, 32, (1, 10), padding=0)
         self.convt_3 = nn.ConvTranspose2d(32, 64, (1, 7), padding=0)
         self.convt_4 = nn.ConvTranspose2d(64, 3, (1, 3), padding=0)
+        
+        self.bn_1 = nn.BatchNorm2d(3)
+        self.bn_2 = nn.BatchNorm2d(16)
+        self.bn_3 = nn.BatchNorm2d(32)
+        self.bn_4 = nn.BatchNorm2d(64)
     
     def forward(self, x):
         x = torch.relu(self.convt_1(x))
+        x = self.bn_2(x)
         x = torch.relu(self.convt_2(x))
+        x = self.bn_3(x)
         x = torch.relu(self.convt_3(x))
+        x = self.bn_4(x)
         x = torch.relu(self.convt_4(x))
+        x = self.bn_1(x)
         
         return x
 
--- main.py
+++ main.py
@@ -1,3 +1,26 @@
 from environment.env import TradingEnv
+from agent.TD3_agent import TD3
+
+import torch
+
+env = TradingEnv()
 
 
+params = {
+    'env': env,
+    'device': torch.device('cuda' if torch.cuda.is_available() else 'cpu'),
+    'GAMMA':0.96,
+    'CRITIC_LR':0.001,
+    'ACTOR_LR':0.001,
+    'TAU': 0.05,
+    'BATCH_SIZE':256,
+    'MEMORY_SIZE': 100000,
+    'EPISODES': 1000,
+    'POLICY_NOISE': 0.2,
+    'NOISE_CLIP': 0.5,
+    'POLICY_DELAY': 2,
+    'EXPLORATION_NOISE':0.1
+}
+
+agent = TD3(**params)
+agent.train(noise='OUNoise')
