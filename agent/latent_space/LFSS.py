e80b7ba0a3d95e64f9e965016de2af6311a6b348
unknown <abab123245@gmail.com>
2022-04-24 15:10:15 +0800

    Update LFSS.py

--- agent/latent_space/LFSS.py
+++ agent/latent_space/LFSS.py
@@ -26,55 +26,16 @@ class TimeSeriesDataset(data.Dataset):
         # please filtered them with KalmanFilter
         ###################################
         
-        class KalmanFilter(object):
-            def __init__(self, F = None, B = None, H = None, Q = None, R = None, P = None, x0 = None):
+        filterdata = []
         
-                if(F is None or H is None):
-                    raise ValueError("Set proper system dynamics.")
-
-                self.n = F.shape[1]
-                self.m = H.shape[1]
-
-                self.F = F
-                self.H = H
-                self.B = 0 if B is None else B
-                self.Q = np.eye(self.n) if Q is None else Q
-                self.R = np.eye(self.n) if R is None else R
-                self.P = np.eye(self.n) if P is None else P
-                self.x = np.zeros((self.n, 1)) if x0 is None else x0
-
-            def predict(self, u = 0):
-                self.x = np.dot(self.F, self.x) + np.dot(self.B, u)
-                self.P = np.dot(np.dot(self.F, self.P), self.F.T) + self.Q
-                return self.x
-
-            def update(self, z):
-                y = z - np.dot(self.H, self.x)
-                S = self.R + np.dot(self.H, np.dot(self.P, self.H.T))
-                K = np.dot(np.dot(self.P, self.H.T), np.linalg.inv(S))
-                self.x = self.x + np.dot(K, y)
-                I = np.eye(self.n)
-                self.P = np.dot(np.dot(I - np.dot(K, self.H), self.P), 
-                    (I - np.dot(K, self.H)).T) + np.dot(np.dot(K, self.R), K.T)
-    
-        def filterresult(measurements):
-        dt = 1.0/60
-        F = np.array([[1, dt, 0], [0, 1, dt], [0, 0, 1]])
-        H = np.array([1, 0, 0]).reshape(1, 3)
-        Q = np.array([[0.05, 0.05, 0.0], [0.05, 0.05, 0.0], [0.0, 0.0, 0.0]])
-        R = np.array([0.5]).reshape(1, 1)
-
-        kf = KalmanFilter(F = F, H = H, Q = Q, R = R)
-        predictions = []
-
-        for z in measurements:
-            predictions.append(np.dot(H,  kf.predict())[0])
-            kf.update(z)
-    
-        return predictions
+        for element in self.X:
+            filterdata.append(np.dot(H,  kf.predict())[0])
+            kf.update(element)
         
-        for index in range(len(self.X)):
-            return self.X[index]
+        filterdata[0] = self.X[0]
+        self.X = filterdata
+        
+        return self.X[index]
 
 
 class PretrainedAEModel:
